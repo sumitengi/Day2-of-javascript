0c867731274799f92a88e49a70bf8d2400e1ec39
sumitengi <=>
2024-11-27 15:14:05 +0530

    practice of javascript operator

---
+++ js/custom.js
@@ -0,0 +1,62 @@
+
+//arthimetic operator
+
+// var a = 10
+// var b = 15
+
+// var c = a+b // add
+// var d = a-b // minus
+// var e = a*b // mutiply
+// var f = a/b // divide
+// var g = a%b // module
+// var c = a++  // post increment add 1 in value
+// var c = ++a // pre increment add 1 in value
+// var d = a-- // post decrement minus 1 in value
+// var d = --a // pre decrement minus 1 in value
+
+// document.write(--a)
+// document.write('<br>')
+// document.write(a)
+
+
+// condition operator
+
+var a = 10
+var b = 15
+
+// var c = a==b
+// var c = a !=b
+// var c = a>b
+// var c = a<b
+// var c = a>=b
+// var c = a<=b
+// document.write(c)
+
+
+// Logical operator
+
+var a = 15
+var b = 55
+var c = 45
+
+// check = a>=18 && b <=50 // output false
+// check = a<=18 || b <=50  // output true
+// document.write(check)
+
+
+// ternary operator
+
+// check =  a<=18 ? "yes":"no"
+// document.write(check)
+
+
+// assigment operator
+
+var a = 10
+// var c = a += 5
+// var c = a -= 5
+// var c = a *= 5
+// var c = a /=5
+// var c = a%= 5
+ 
+console.log(c)
